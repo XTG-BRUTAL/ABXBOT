f9e17f9fab9765adee35c473c2d4699b838e0894
XTG BRUTAL <92993386+XTG-BRUTAL@users.noreply.github.com>
2021-11-16 15:06:32 +0530

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -2,16 +2,16 @@ let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let fetch = require('node-fetch')
 let handler = async (m, { args, usedPrefix, command }) => {
 
-    if (!args[0]) throw 'link githubnya mana? contoh: https://github.com/Bintang73/botst4rz'
+    if (!args[0]) throw 'link where is github? example: https://github.com/XTG-BRUTAL/BotOfBrutal'
 
-    if (!regex.test(args[0])) throw 'link salah!'
+    if (!regex.test(args[0])) throw 'wrong link!'
 
     let [, user, repo] = args[0].match(regex) || []
     repo = repo.replace(/.git$/, '')
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
     let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
     // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
-    m.reply(`*Mohon tunggu, sedang mengirim repository..*`)
+    m.reply(`*Please wait, sending repository..*`)
     conn.sendFile(m.chat, url, filename, null, m)
 
 }
