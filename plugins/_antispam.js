d4d8ff37d23ac4cd09f309ba7273801e5e41e85e
XTG BRUTAL <92993386+XTG-BRUTAL@users.noreply.github.com>
2021-11-16 13:59:29 +0530

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -7,7 +7,7 @@ module.exports = {
             if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
                 if (this.spam[m.sender].count > 10) {
                     //global.db.data.users[m.sender].banned = true
-                    m.reply('*Jangan Spam!!*')
+                    m.reply('*STOP SPAMMING!!*')
                 }
                 this.spam[m.sender].count = 0
                 this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
