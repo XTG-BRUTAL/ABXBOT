d86561e69eb7e75bb80d2b4a15bd7dc7fba3a032
XTG BRUTAL <92993386+XTG-BRUTAL@users.noreply.github.com>
2021-11-17 14:53:06 +0530

    Update blockList.js

--- plugins/blockList.js
+++ plugins/blockList.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
     let blocked = conn.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
     conn.reply(m.chat, (`
-┌ *「 Daftar Terblokir 」*` + blocked.map((v, i) => `
+┌ *「 Blocked List 」*` + blocked.map((v, i) => `
 │ ${i + 1}. @${v.split`@`[0]}`).join`` + `
 └────`).trim() , m, { contextInfo: { mentionedJid: blocked } })
 }
